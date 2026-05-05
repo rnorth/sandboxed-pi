9ab0f5fefa3a24d6f7354f34aa54a58925e328e9
Richard North <richard.north@gmail.com>
2026-05-05 21:07:33 +0100

    fix: move pytest to test-runtime install, fix recursive gitignore for __pycache__

--- scripts/test-proxy.sh
+++ scripts/test-proxy.sh
@@ -2,4 +2,4 @@
 set -e
 docker build -t pi-egress-proxy:test ./proxy
 docker run --rm --entrypoint sh -v "$(pwd)/proxy:/proxy" pi-egress-proxy:test \
-  -c "cd /proxy && python -m pytest tests/ -v"
+  -c "pip install -q pytest && cd /proxy && python -m pytest tests/ -v"
